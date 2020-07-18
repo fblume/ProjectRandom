92d80742ed0c8194cd7200e3a097c735bd0d0ca2
fblume <67295045+fblume@users.noreply.github.com>
2020-07-18 10:51:08 -0500

    Delete 2_Random_migration.js

--- ProjectRandom/migrations/2_Random_migration.js
+++
@@ -1,9 +0,0 @@
-const Random = artifacts.require("Random");
-const BigNumber = require('bignumber.js');
-
-module.exports = function(deployer, networks, accounts) {
-  deployer.deploy(Random).then(async function(instance){
-    await instance.receiveMoney({from: accounts[1], value: web3.utils.toWei("4", "ether")});
-    await instance.sendMoney(accounts[3], 1000000, {from: accounts[0]});
-  });
-}
