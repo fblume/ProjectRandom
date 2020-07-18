2873b401227f342fcc5dd796ebde6a7ee9fd3219
fblume <67295045+fblume@users.noreply.github.com>
2020-07-18 11:12:45 -0500

    Add files via upload

---
+++ ProjectRandom/migrations/2_Random_migration.js
@@ -0,0 +1,10 @@
+const Random = artifacts.require("Random");
+const BigNumber = require('bignumber.js');
+var randomValue = 0;
+var totalGain;
+
+module.exports = function(deployer, networks, accounts) {
+  deployer.deploy(Random).then(async function(instance){
+    await instance.initialDeposit({from: accounts[0], value: web3.utils.toWei("10", "ether")});
+  });
+}
